628af528bbee205f8a2a28d85f9a033356b17bad
CrafterKina <kina_ugo@yahoo.co.jp>
2015-01-02 09:54:05 +0900

    close #6 アイテム・ブロックの登録関係が小回りがきかない

--- src/main/java/com/mods/kina/ExperiencePower/collection/EnumEPBlock.java
+++ src/main/java/com/mods/kina/ExperiencePower/collection/EnumEPBlock.java
@@ -2,6 +2,7 @@ package com.mods.kina.ExperiencePower.collection;
 
 import com.mods.kina.ExperiencePower.block.*;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
 
 /**
  ExperiencePowerのBlockをひたすら列挙するクラス。
@@ -15,13 +16,50 @@ public enum EnumEPBlock{
     ExperienceCrop(new BlockExpWheat()),
     ExperienceFurnace(new BlockExpFurnace());
 
+    private Class<? extends ItemBlock> itemBlock;
     private Block block;
+    private int modelCount;
+    private String[] modelNames;
 
     EnumEPBlock(Block block){
         this.block=block;
+        itemBlock = ItemBlock.class;
+        modelCount = 1;
+    }
+
+    EnumEPBlock(Block block, Class<? extends ItemBlock> itemBlock){
+        this(block);
+        this.itemBlock = itemBlock;
+    }
+
+    EnumEPBlock(Block block, Class<? extends ItemBlock> itemBlock, int modelCount){
+        this(block, itemBlock);
+        this.modelCount = modelCount;
+    }
+
+    EnumEPBlock(Block block, Class<? extends ItemBlock> itemBlock, String... modelNames){
+        this(block, itemBlock);
+        modelCount = modelNames.length;
+        this.modelNames = modelNames;
     }
 
     public Block getBlock(){
         return block;
+    }
+
+    public Class<? extends ItemBlock> getItemBlock(){
+        return itemBlock;
+    }
+
+    public int getModelCount(){
+        return modelCount;
+    }
+
+    public String getBlockName(){
+        return block.getUnlocalizedName().substring(5);
+    }
+
+    public String[] getModelNames(){
+        return modelNames;
     }
 }
--- src/main/java/com/mods/kina/ExperiencePower/register/BlockRegistrar.java
+++ src/main/java/com/mods/kina/ExperiencePower/register/BlockRegistrar.java
@@ -3,6 +3,7 @@ package com.mods.kina.ExperiencePower.register;
 import com.mods.kina.ExperiencePower.collection.EnumEPBlock;
 import com.mods.kina.ExperiencePower.collection.StaticFieldCollection;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -13,14 +14,28 @@ public class BlockRegistrar{
 
     public static void registerBlock(){
         for(EnumEPBlock epBlock:EnumEPBlock.values()){
-            GameRegistry.registerBlock(epBlock.getBlock(),epBlock.getBlock().getUnlocalizedName().substring(5));
+            GameRegistry.registerBlock(epBlock.getBlock(), epBlock.getItemBlock(), epBlock.getBlockName());
         }
     }
 
     @SideOnly(Side.CLIENT)
     public static void registerModel(){
         for(EnumEPBlock epBlock:EnumEPBlock.values()){
-            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(epBlock.getBlock()), 0, new ModelResourceLocation(StaticFieldCollection.MODID + ":" + epBlock.getBlock().getUnlocalizedName().substring(5), "inventory"));
+            if(epBlock.getModelCount() == 1){
+                Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(epBlock.getBlock()), 0, new ModelResourceLocation(StaticFieldCollection.MODID + ":" + epBlock.getBlockName(), "inventory"));
+            }else{
+                if(epBlock.getModelNames().length > 0){
+                    ModelBakery.addVariantName(Item.getItemFromBlock(epBlock.getBlock()), epBlock.getModelNames());
+                    for(int i = 0; i < epBlock.getModelCount(); i++){
+                        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(epBlock.getBlock()), i, new ModelResourceLocation(epBlock.getModelNames()[i], "inventory"));
+                    }
+                }else{
+                    for(int i = 0; i < epBlock.getModelCount(); i++){
+                        ModelBakery.addVariantName(Item.getItemFromBlock(epBlock.getBlock()), StaticFieldCollection.MODID + ":" + epBlock.getBlockName() + "_" + i);
+                        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(epBlock.getBlock()), i, new ModelResourceLocation(StaticFieldCollection.MODID + ":" + epBlock.getBlockName() + "_" + i, "inventory"));
+                    }
+                }
+            }
         }
     }
 }
