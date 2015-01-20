a70b18022672c0277780d86abb16e4565abb0fd2
CrafterKina <kina_ugo@yahoo.co.jp>
2015-01-20 21:56:52 +0900

    #19 レンチの実装をしよう 追Push

--- src/main/java/com/mods/kina/ExperiencePower/collection/EnumEPItem.java
+++ src/main/java/com/mods/kina/ExperiencePower/collection/EnumEPItem.java
@@ -3,6 +3,7 @@ package com.mods.kina.ExperiencePower.collection;
 import com.mods.kina.ExperiencePower.item.ItemEPMonsterPlacer;
 import com.mods.kina.ExperiencePower.item.ItemExpSealableBook;
 import com.mods.kina.ExperiencePower.item.ItemIngot;
+import com.mods.kina.ExperiencePower.item.ItemWrenchWand;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemSeeds;
@@ -11,7 +12,8 @@ public enum EnumEPItem{
     ExperienceSealableBook(new ItemExpSealableBook()),
     ExperienceCropSeed(new ItemSeeds(EnumEPBlock.ExperienceCrop.getBlock(), Blocks.farmland).setUnlocalizedName("exp_seed").setCreativeTab(EnumEPCreativeTab.ITEM.getCreativeTab())),
     Ingot(new ItemIngot()),
-    MonsterPlacer(new ItemEPMonsterPlacer());
+    MonsterPlacer(new ItemEPMonsterPlacer()),
+    Wrench(new ItemWrenchWand());
     private Item item;
     EnumEPItem(Item item){
         this.item=item;
